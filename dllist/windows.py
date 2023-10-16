992d81a52e8ff2b727af6344436059a2267aabfe
Brian Ward <bward@flatironinstitute.org>
2023-10-16 10:47:50 -0400

    Windows: Use alternative enumeration scheme

--- dllist/windows.py
+++ dllist/windows.py
@@ -1,55 +1,90 @@
 import ctypes
 import warnings
-from typing import List
+from ctypes.wintypes import BOOL, DWORD, HANDLE, HMODULE, LPDWORD, LPWSTR
+from typing import List, Optional, Tuple
 
-# https://learn.microsoft.com/en-us/windows/win32/api/dbghelp/nf-dbghelp-enumerateloadedmodules64
+# https://learn.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocessmodulesex
 
-# fmt: off
-LPVOID                          = ctypes.c_void_p
-PVOID                           = LPVOID
-HANDLE                          = LPVOID
-BOOL                            = ctypes.c_int32
-DWORD                           = ctypes.c_uint32
-PCSTR                           = ctypes.c_char_p
-ULONG                           = ctypes.c_uint32
-PENUMLOADED_MODULES_CALLBACK64  = ctypes.WINFUNCTYPE(BOOL, PCSTR, ULONG, ULONG, ctypes.POINTER(ctypes.py_object))
-# fmt: on
+LIST_MODULES_ALL = 3  # 0x03
 
 
-@PENUMLOADED_MODULES_CALLBACK64
-def enum_modules_callback(module_name, _module_base, _module_size, data):
-    libraries = data.contents.value
-
-    try:
-        name = module_name.decode("utf-8")
-        libraries.append(name)
-    except:
-        warnings.warn(f"Could not decode library name {module_name}")
-    return int(True)
+def get_current_process() -> HANDLE:
+    _get_current_process = ctypes.windll.kernel32.GetCurrentProcess
+    _get_current_process.restype = HANDLE
+    return _get_current_process()
 
 
-def _platform_specific_dllist() -> List[str]:
-    # could have issues on WINE
-    # see https://github.com/JuliaLang/julia/pull/33062
-    get_current_process = ctypes.windll.kernel32.GetCurrentProcess
-    get_current_process.restype = HANDLE
-    process = ctypes.windll.kernel32.GetCurrentProcess()
+def get_module_filename(hModule: HMODULE) -> Optional[str]:
+    _get_module_filename = ctypes.windll.kernel32.GetModuleFileNameW
+    _get_module_filename.argtypes = [HMODULE, LPWSTR, DWORD]
+    _get_module_filename.restype = DWORD
 
-    libraries = []
-    enumerate_loaded_modules = ctypes.windll.dbghelp.EnumerateLoadedModules64
-    enumerate_loaded_modules.argtypes = [
+    nSize = 32768  # MAX_PATH
+    lpFilename = ctypes.create_unicode_buffer(nSize)
+    if _get_module_filename(hModule, lpFilename, nSize) != 0:
+        # encoding?
+        return lpFilename.value
+    else:
+        warnings.warn(
+            f"Failed to get module file name for module {hModule}", stacklevel=2
+        )
+
+
+def get_process_module_handles_partial(
+    hProcess: HANDLE, maxbuffsize: int
+) -> Tuple[List[HMODULE], int]:
+    _enumerate_loaded_modules = ctypes.windll.psapi.EnumProcessModulesEx
+    _enumerate_loaded_modules.argtypes = [
         HANDLE,
-        PENUMLOADED_MODULES_CALLBACK64,
-        ctypes.POINTER(ctypes.py_object),
+        ctypes.POINTER(HMODULE),
+        DWORD,
+        LPDWORD,
+        DWORD,
     ]
-    success = enumerate_loaded_modules(
-        process, enum_modules_callback, ctypes.pointer(ctypes.py_object(libraries))
+    _enumerate_loaded_modules.restype = BOOL
+    cb = DWORD(maxbuffsize * ctypes.sizeof(HMODULE))
+    cbNeeded = DWORD(0)
+
+    hModules = (HMODULE * maxbuffsize)()
+    success = _enumerate_loaded_modules(
+        hProcess,
+        hModules,
+        cb,
+        ctypes.pointer(cbNeeded),
+        LIST_MODULES_ALL,
     )
+
     if not success:
         warnings.warn(
             "Unable to list loaded libraries: "
-            f"EnumerateLoadedModules64 failed with error code {ctypes.GetLastError()}"
+            f"EnumProcessModulesEx failed with error code {ctypes.GetLastError()}",
+            stacklevel=3,
         )
+        return [], 0
+
+    bufsize_needed = cbNeeded.value // ctypes.sizeof(HMODULE)
+
+    return hModules, bufsize_needed
+
+
+def get_process_module_handles() -> List[HMODULE]:
+    hProcess = get_current_process()
+    hModules, cb_needed = get_process_module_handles_partial(hProcess, maxbuffsize=1024)
+    if cb_needed > 1024:
+        # retry with larger buffer
+        hModules, _ = get_process_module_handles_partial(
+            hProcess, maxbuffsize=cb_needed
+        )
+    return hModules
+
+
+def _platform_specific_dllist() -> List[str]:
+    hModules = get_process_module_handles()
+
+    libraries = [
+        name for hMod in hModules if (name := get_module_filename(hMod)) is not None
+    ]
+
     if libraries:
         # remove the first entry, which is the executable itself
         libraries.pop(0)
